99f295acf17272637483f6a0e5483b72db54e1f3
Heez27 <ohhj6835@gmail.com>
2021-04-15 14:25:18 +0900

    Initial commit

---
+++ ch 4. 구현/2. 시각.py
@@ -0,0 +1,9 @@
+n = input('N시의 N을 입력하시오: ')
+print(n.find('3'))
+for i in range(int(n)):
+    if (n+1)
+# hour3 = n // 3 # hour에 3의 배수 갯수
+# minute3 = 59 // 3
+# second3 = 60 // 3
+#
+# print(hour3*minute3*second3)
